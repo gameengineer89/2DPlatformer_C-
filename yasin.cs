933948a4cc9cb088f7d916844d9f4aedf7737d01
gameengineer89 <gameengineer89@gmail.com>
2021-06-13 04:46:29 +0700

    [master]update gitignore and mono

--- yasin.cs
+++ yasin.cs
@@ -5,6 +5,13 @@ public class yasin : KinematicBody2D
 {
     // Declare member variables here. Examples:
     private Vector2 _velocity = new Vector2();
+    private static readonly Vector2 up = new Vector2 (0, 1);
+
+    // private static readonly Vector2 down   = new Vector2 (0, -1);
+    // private static readonly Vector2 right  = new Vector2 (1, 0);
+    // private static readonly Vector2 left   = new Vector2 (-1, 0);
+
+
     const int SPEEED = 180;
     const int GRAVITY =35;
     const int JUMPFORCE = -1100;
@@ -12,20 +19,21 @@ public class yasin : KinematicBody2D
 
     public override void _PhysicsProcess(float _delta)
     {
-       if (Input.IsActionPressed("right")){
-           _velocity.x = SPEEED;
-       }
+        if (Input.IsActionPressed("right")){
+            _velocity.x = SPEEED;
+        }
 
-       if (Input.IsActionPressed("left")){
-           _velocity.x = -SPEEED;
-       }
+        if (Input.IsActionPressed("left")){
+            _velocity.x = -SPEEED;
+        }
 
-       if (Input.IsActionJustPressed("jump")){
-           _velocity.y = JUMPFORCE;
-       }
+        if (Input.IsActionJustPressed("jump")){
+            _velocity.y = JUMPFORCE;
+        }
 
-       _velocity = MoveAndCollide(_velocity);
+        _velocity.y = _velocity.y + GRAVITY;
 
+        _velocity = MoveAndSlide(_velocity, up);
 
         _velocity.x = Mathf.Lerp(_velocity.x, 0, 2);
             
